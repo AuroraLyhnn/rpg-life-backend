cf44bd6311cc483142d19a1a26781dfceb03ef23
AuroraLyhnn <sisserahvds@gmail.com>
2025-07-29 13:55:41 +0200

    Initial commit

---
+++ src/main/java/com/aurora_lyhnn/rpg_life/RpgLifeApplication.java
@@ -0,0 +1,13 @@
+package com.aurora_lyhnn.rpg_life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aurora_lyhnn/rpg_life/RpgLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aurora_lyhnn.rpg_life;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
