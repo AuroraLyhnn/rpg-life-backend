df72ca804600f1454892df3541a22bd18e2af46d
AuroraLyhnn <sisserahvds@gmail.com>
2025-08-22 10:37:41 +0200

    I've been working on the quest Feature, I made the mappers and service. The queries in the repository are quite long, but i think it needs that

---
+++ src/main/java/com/aurora_lyhnn/rpg_life/controller/QuestController.java
@@ -0,0 +1,14 @@
+package com.aurora_lyhnn.rpg_life.controller;
+
+import com.aurora_lyhnn.rpg_life.service.QuestService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("quests")
+public class QuestController {
+
+
+
+}
